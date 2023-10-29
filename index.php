f89e2fa398bd76e7b25e4d771c40d626a1ea3048
Mathis-Aubry <72527059+Mathis-Aubry@users.noreply.github.com>
2023-10-29 18:52:33 +0100

    test router #3

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <script src="script/main.js"></script>
         <?php
             $pageTitle = 'Page Personnelle';
-            $pageFile = 'home.php';
+            $pageFile = 'home.html';
             if ($_SERVER['REQUEST_URI'] != '/') {
                 // Routing all projects
                 if (strpos($_SERVER['REQUEST_URI'], 'projets')) {
@@ -28,10 +28,71 @@
         <title>Mathis Aubry | <?= $pageTitle ?></title>
     </head>
     <body>
-        <?php include 'header.html' ?>
+        <header>
+            <div>
+                <div>
+                    <a href="/">Page Personnelle de Mathis Aubry</a>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="#home">Accueil</a></li>
+                        <li><a href="#skills">Compétences</a></li>
+                        <li><a href="#projects">Projets</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </nav>
+                <a href="">
+                    <i class="icon-list5"></i>
+                </a>
+            </div>
+        </header>
         <main>
             <?php include $pageFile ?>
         </main>
-        <?php include 'footer.html' ?>
+        <footer id="contact">
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none">
+                <path d="M0 0 C 50 100 80 100 100 0 Z"></path>
+            </svg>
+            <div class="cleanWrap">
+                <address>
+                    <h3>Contact</h3>
+                    <ul>
+                        <li>
+                            <a href="https://goo.gl/maps/rKc8L2kAbbKVLMAU9" target="_blank" rel="noopener noreferrer"><i class="icon-house"></i>&emsp;Livré Sur Changeon 35450</a>
+                        </li>
+                        <li>
+                            <a href="https://iut-lannion.univ-rennes1.fr/" target="_blank" rel="noopener noreferrer"><i class="icon-study"></i>&emsp;IUT Lannion</a>
+                        </li>
+                    </ul>
+                </address>
+                <nav>
+                    <h3>Navigation</h3>
+                    <ul>
+                        <li>
+                            <a href="#home">Accueil&emsp;<i class="icon-circles"></i></a>
+                        </li>
+                        <li>
+                            <a href="#skills">Compétences&emsp;<i class="icon-circles"></i></a>
+                        </li>
+                        <li>
+                            <a href="#projects">Projets&emsp;<i class="icon-circles"></i></a>
+                        </li>
+                        <li>
+                            <a href="#contact">Contact&emsp;<i class="icon-circles"></i></a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <p></p>
+        </footer>
+        <div id="loader-overlay">
+            <div id="loader">
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+            </div>
+        </div>
     </body>
 </html>
